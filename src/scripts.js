d049af970df483cee8bfca880927972bed46541d
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2024-08-10 23:39:18 -0500

    feat: Responsive form & Form validation with ts

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,80 @@
 "use strict";
-const button = document.getElementById('button');
-const fetching = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-        .then(response => {
-        return response.json();
-    })
-        .then(data => {
-        console.log(data);
-    });
+// declare const axios: any;
+/* 1. Extraer los valores de los campos */
+const form = document.getElementById('form');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const age = document.getElementById('age');
+const description = document.getElementById('description');
+const password = document.getElementById('password');
+const repeatPassword = document.getElementById('repeatPassword');
+const result = document.getElementById('result');
+const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
 };
-button === null || button === void 0 ? void 0 : button.addEventListener('click', async () => {
-    fetching();
+/* 3. Crea un EventListener en el formulario que ejecute una función al enviarlo */
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    /* 4. Realiza una validación de la entrada de los datos */
+    if (firstname.value.length < 3) {
+        alert('Firstname must be at least 3 characters');
+        return;
+    }
+    if (lastname.value.length < 3) {
+        alert('Lastname must be at least 3 characters');
+        return;
+    }
+    if (email.value.length < 3) {
+        alert('Email must be at least 3 characters');
+        return;
+    }
+    if (!validateEmail(email.value)) {
+        alert('Email is not valid');
+        return;
+    }
+    if (phone.value.length < 9) {
+        alert('Phone must be at least 9 characters');
+        return;
+    }
+    if (parseInt(age.value) < 13) {
+        alert('Age must be at least 13');
+        return;
+    }
+    if (description.value.length < 10) {
+        alert('Description must be at least 10 characters');
+        return;
+    }
+    if (password.value.length < 6) {
+        alert('Password must be at least 6 characters');
+        return;
+    }
+    if (password.value !== repeatPassword.value) {
+        alert('Passwords do not match');
+        return;
+    }
+    const user = {
+        firstname: firstname === null || firstname === void 0 ? void 0 : firstname.value,
+        lastname: lastname === null || lastname === void 0 ? void 0 : lastname.value,
+        email: email === null || email === void 0 ? void 0 : email.value,
+        phone: phone === null || phone === void 0 ? void 0 : phone.value,
+        age: parseInt(age === null || age === void 0 ? void 0 : age.value),
+        description: description === null || description === void 0 ? void 0 : description.value,
+        password: password === null || password === void 0 ? void 0 : password.value
+    };
+    /* 5. Imprimir en consola los datos rellenados*/
+    console.log(user);
+    /* 6. Muestra los datos rellenados en el documento HTML */
+    result.innerHTML = `
+    <h2 class="text-xl font-bold">User Info</h2>
+    <p><b>Firstname:</b> ${user.firstname}</p>
+    <p><b>Lastname:</b> ${user.lastname}</p>
+    <p><b>Email:</b> ${user.email}</p>
+    <p><b>Phone:</b> ${user.phone}</p>
+    <p><b>Age:</b> ${user.age}</p>
+    <p><b>Description:</b> ${user.description}</p>
+    <p><b>Password:</b> ${user.password}</p>
+  `;
 });
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,17 +1,105 @@
+// declare const axios: any;
 
-const button: HTMLElement | null = document.getElementById('button');
+/* 1. Extraer los valores de los campos */
+const form: HTMLFormElement | null = document.getElementById('form') as HTMLFormElement;
+const firstname: HTMLInputElement | null = document.getElementById('firstname') as HTMLInputElement;
+const lastname: HTMLInputElement | null = document.getElementById('lastname') as HTMLInputElement;
+const email: HTMLInputElement | null = document.getElementById('email') as HTMLInputElement;
+const phone: HTMLInputElement | null = document.getElementById('phone') as HTMLInputElement;
+const age: HTMLInputElement | null = document.getElementById('age') as HTMLInputElement;
+const description: HTMLInputElement | null = document.getElementById('description') as HTMLInputElement;
+const password: HTMLInputElement | null = document.getElementById('password') as HTMLInputElement;
+const repeatPassword: HTMLInputElement | null = document.getElementById('repeatPassword') as HTMLInputElement;
+const result: HTMLElement | null = document.getElementById('result') as HTMLElement;
 
-const fetching = () => {
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-    })
+/* 2. Crea una interfaz con la forma y tipo de datos de los campos del formulario */
+interface User {
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  age: number;
+  description: string;
+  password: string;
 }
 
-button?.addEventListener('click', async () => {
-  fetching();
-});
+const validateEmail = (email: string): boolean => {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
 
+/* 3. Crea un EventListener en el formulario que ejecute una función al enviarlo */
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  /* 4. Realiza una validación de la entrada de los datos */
+  if (firstname.value.length < 3) {
+    alert('Firstname must be at least 3 characters');
+    return;
+  }
+
+  if (lastname.value.length < 3) {
+    alert('Lastname must be at least 3 characters');
+    return;
+  }
+
+  if (email.value.length < 3) {
+    alert('Email must be at least 3 characters');
+    return;
+  }
+
+  if (!validateEmail(email.value)) {
+    alert('Email is not valid');
+    return;
+  }
+
+  if (phone.value.length < 9) {
+    alert('Phone must be at least 9 characters');
+    return;
+  }
+
+  if (parseInt(age.value) < 13) {
+    alert('Age must be at least 13');
+    return;
+  }
+
+  if (description.value.length < 10) {
+    alert('Description must be at least 10 characters');
+    return;
+  }
+
+  if (password.value.length < 6) {
+    alert('Password must be at least 6 characters');
+    return;
+  }
+
+  if (password.value !== repeatPassword.value) {
+    alert('Passwords do not match');
+    return;
+  }
+
+  const user: User = {
+    firstname: firstname?.value,
+    lastname: lastname?.value,
+    email: email?.value,
+    phone: phone?.value,
+    age: parseInt(age?.value),
+    description: description?.value,
+    password: password?.value
+  }
+
+  /* 5. Imprimir en consola los datos rellenados*/
+  console.log(user);
+
+  /* 6. Muestra los datos rellenados en el documento HTML */
+  result.innerHTML = `
+    <h2 class="text-xl font-bold">User Info</h2>
+    <p><b>Firstname:</b> ${user.firstname}</p>
+    <p><b>Lastname:</b> ${user.lastname}</p>
+    <p><b>Email:</b> ${user.email}</p>
+    <p><b>Phone:</b> ${user.phone}</p>
+    <p><b>Age:</b> ${user.age}</p>
+    <p><b>Description:</b> ${user.description}</p>
+    <p><b>Password:</b> ${user.password}</p>
+  `;
+});

2d96fd5f21ffebe56ae5f568105c6389e86675e8
mateonoel2 <mateo.noel@utec.edu.pe>
2024-10-02 08:49:47 -0500

    a

--- src/scripts.js
+++ src/scripts.js
@@ -1,28 +1,39 @@
 import { axiosPost } from "./api.js";
 const form = document.getElementById("form");
-const firstname = document.getElementById('firstname');
-const lastname = document.getElementById('lastname');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-const age = document.getElementById('age');
-const description = document.getElementById('description');
-const password = document.getElementById('password');
+const fields = [
+    "firstname",
+    "lastname",
+    "email",
+    "phone",
+    "age",
+    "description",
+    "password",
+];
+const [firstname, lastname, email, phone, age, description, password] = fields.map((id) => document.getElementById(id));
 form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
     e.preventDefault();
     const student = {
         id: null,
-        firstname: firstname === null || firstname === void 0 ? void 0 : firstname.value,
-        lastname: lastname === null || lastname === void 0 ? void 0 : lastname.value,
-        email: email === null || email === void 0 ? void 0 : email.value,
-        phone: phone === null || phone === void 0 ? void 0 : phone.value,
-        age: Number.parseInt(age === null || age === void 0 ? void 0 : age.value),
-        description: description === null || description === void 0 ? void 0 : description.value,
-        password: password === null || password === void 0 ? void 0 : password.value
+        firstname: (firstname === null || firstname === void 0 ? void 0 : firstname.value) || "",
+        lastname: (lastname === null || lastname === void 0 ? void 0 : lastname.value) || "",
+        email: (email === null || email === void 0 ? void 0 : email.value) || "",
+        phone: (phone === null || phone === void 0 ? void 0 : phone.value) || "",
+        age: Number.parseInt((age === null || age === void 0 ? void 0 : age.value) || "0"),
+        description: (description === null || description === void 0 ? void 0 : description.value) || "",
+        password: (password === null || password === void 0 ? void 0 : password.value) || "",
     };
     console.log(student);
     async function callPostStudent(student) {
-        const res = await axiosPost(student);
-        console.log(res);
+        try {
+            const res = await axiosPost(student);
+            if (res.status === 201) {
+                alert("Student created successfully!");
+                console.log(res);
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
     }
     callPostStudent(student);
 });

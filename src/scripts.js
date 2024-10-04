bac29a47205fed216b65c1c4da54d94b0c470af1
mateonoel2 <mateo.noel@utec.edu.pe>
2024-10-04 08:00:44 -0500

    update

--- src/scripts.js
+++ src/scripts.js
@@ -10,7 +10,30 @@ const fields = [
     "password",
 ];
 const [firstname, lastname, email, phone, age, description, password] = fields.map((id) => document.getElementById(id));
+const BACKEND_URL = "http://localhost:8080";
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+async function axiosPost(student) {
+    try {
+        const response = await axios.post(`${BACKEND_URL}/student`, student);
+        console.log(response);
+        // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
 form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
     e.preventDefault();
-    alert(firstname === null || firstname === void 0 ? void 0 : firstname.value);
+    const student = {
+        id: null,
+        firstname: (firstname === null || firstname === void 0 ? void 0 : firstname.value) || "",
+        lastname: (lastname === null || lastname === void 0 ? void 0 : lastname.value) || "",
+        email: (email === null || email === void 0 ? void 0 : email.value) || "",
+        phone: (phone === null || phone === void 0 ? void 0 : phone.value) || "",
+        age: Number.parseInt((age === null || age === void 0 ? void 0 : age.value) || "0"),
+        description: (description === null || description === void 0 ? void 0 : description.value) || "",
+        password: (password === null || password === void 0 ? void 0 : password.value) || "",
+    };
+    console.log(student);
+    axiosPost(student);
 });
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,7 +1,6 @@
 const form: HTMLFormElement | null = document.getElementById(
   "form"
 ) as HTMLFormElement;
-
 const fields = [
   "firstname",
   "lastname",
@@ -13,9 +12,49 @@ const fields = [
 ];
 const [firstname, lastname, email, phone, age, description, password] =
   fields.map((id) => document.getElementById(id) as HTMLInputElement | null);
+  
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  declare const axios: any;
+
+const BACKEND_URL: string = "http://localhost:8080";
+
+interface Student {
+  id: number | null;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  age: number;
+  description: string;
+  password: string;
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+async function axiosPost(student: Student): Promise<any> {
+  try {
+    const response = await axios.post(`${BACKEND_URL}/student`, student);
+    console.log(response);
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  } catch (error: any) {
+    console.error(error);
+  }
+}
 
 form?.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  alert(firstname?.value)
+  const student: Student = {
+    id: null,
+    firstname: firstname?.value || "",
+    lastname: lastname?.value || "",
+    email: email?.value || "",
+    phone: phone?.value || "",
+    age: Number.parseInt(age?.value || "0"),
+    description: description?.value || "",
+    password: password?.value || "",
+  };
+
+  console.log(student);
+
+  axiosPost(student);
 });
